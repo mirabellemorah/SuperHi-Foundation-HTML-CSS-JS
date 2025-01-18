9e6ea476d287c6a087f42497f196fb4decddca83
Mirabelle Morah <myrakuti@gmail.com>
2025-01-18 07:53:40 +0000

    Baker and brown added

---
+++ Week 6/main.js
@@ -0,0 +1,16 @@
+$(".menu-toggle").hover(function () {
+
+    $(".menu").toggleClass("open")
+
+    return false
+
+})
+
+
+/* $(".menu-toggle").on("click", function() {
+
+$(".menu").toggleClass("open")
+
+return false
+
+}) */
